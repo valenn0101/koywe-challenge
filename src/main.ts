4bc7e947021fa55696b80a8dd25344efe52f9664
valenn0101 <veroncaceresvalentin@gmail.com>
2025-03-21 12:33:19 +0000

    feat(filters): create exceptions and validation filters

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,32 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: (errors) => {
+        const messages = errors.map((err) => {
+          return {
+            property: err.property,
+            constraints: Object.values(err.constraints || {}),
+          };
+        });
+
+        return {
+          name: 'ValidationError',
+          message: 'Error de validación',
+          response: {
+            message: messages,
+          },
+        };
+      },
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
