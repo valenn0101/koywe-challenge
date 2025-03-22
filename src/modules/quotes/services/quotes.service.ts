13a268fa62d8cfc941fa5e993340957986032126
valenn0101 <veroncaceresvalentin@gmail.com>
2025-03-22 21:40:32 +0000

    feat(Quotes): add get methods

--- src/modules/quotes/services/quotes.service.ts
+++ src/modules/quotes/services/quotes.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, NotFoundException } from '@nestjs/common';
 import { QuotesRepository } from '../repositories/quotes.repository';
 import { CreateQuoteDto } from '../dto/create-quote.dto';
 import {
@@ -11,6 +11,8 @@ import {
   IExchangeRateApi,
   EXCHANGE_RATE_API,
 } from '../infrastructure/api/exchange-rate-api.interface';
+import { Currency } from '../interfaces/currency.enum';
+import { QuoteEntity } from '../entities/quote.entity';
 
 @Injectable()
 export class QuotesService {
@@ -63,5 +65,29 @@ export class QuotesService {
 
       throw new QuoteCreationFailedException();
     }
+  }
+
+  async getQuoteById(id: number): Promise<IQuoteResponse | null> {
+    const quote = await this.quotesRepository.findById(id);
+
+    if (!quote) {
+      throw new NotFoundException(`Cotización con ID ${id} no encontrada`);
+    }
+
+    const now = new Date();
+    if (now > quote.expiresAt) {
+      throw new NotFoundException(`La cotización con ID ${id} ha expirado`);
+    }
+
+    const { userId, createdAt, updatedAt, ...result } = quote;
+    return result;
+  }
+
+  async getAllCurrencies(): Promise<Currency[]> {
+    return Object.values(Currency);
+  }
+
+  async getUserQuotes(userId: number): Promise<QuoteEntity[]> {
+    return this.quotesRepository.findByUserId(userId);
   }
 }
